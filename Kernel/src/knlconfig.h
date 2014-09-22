81007ae8a7f391140b8a3307ca70b4f47cd1ef95
Guilherme Schardong <schardon@tecgraf.puc-rio.br>
2014-09-22 09:42:19 -0300

    Added the missing knlconfig.h file.

---
+++ Kernel/src/knlconfig.h
@@ -0,0 +1,12 @@
+#ifndef KNLCONFIG_H
+#define KNLCONFIG_H
+
+#include "dataset.h"
+#include "tfunction.h"
+#include "logger.h"
+#include "datahandler.h"
+#include "datatransfer.h"
+#include "datasetmanager.h"
+#include "tfmanager.h"
+
+#endif // KNLCONFIG_H
