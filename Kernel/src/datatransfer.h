fb7f940e30668f61c1fb421b8c7f5459e2c5d6d7
Guilherme G. Schardong <schardong.gg@gmail.com>
2014-11-29 16:00:13 -0200

    Added the documentation for the Upload1DTexture function

--- Kernel/src/datatransfer.h
+++ Kernel/src/datatransfer.h
@@ -17,29 +17,30 @@ namespace knl
       /*!
        \brief Allocates an one dimensional texture.
 
-       This function request a texture name to the OpenGL context using the glGenTextures function,
-       allocates the space and sets the parameters (MIN, MAG filterns and WRAP_S mode).
-       The format and internal format used in the glTexImage1D function are calculated using the
-       number of channels given.
+       This function request a texture name to the OpenGL context using the
+       glGenTextures function, allocates the space and sets the parameters (MIN,
+       MAG filters and WRAP_S mode). The format and internal format used in the
+       glTexImage1D function are calculated using the number of channels given.
 
-       The texture data is always GL_UNSIGNED_BYTE. This function is used mainly to allocate transfer
-       function data.
+       The texture data is always GL_UNSIGNED_BYTE. This function is used mainly
+       to allocate transfer function data.
 
        \param w Width of the texture.
        \param num_channels Number o channels used.
-       \return GLuint The texture id given by the OpenGL context (glGenTextures function).
+       \return GLuint The texture id given by the OpenGL context (glGenTextures
+               function).
       */
       GLuint Alloc1DTex(size_t w, size_t num_channels);
 
       /*!
        \brief Allocates a 2D texture.
 
-       Same as the Alloc1DTex function. The OpenGL texture id given by the glGenTextures function.
-       The space is allocated and parameters (MIN, MAG, WRAP_*) set in the same ways as the
-       Alloc1DTex function.
+       Same as the Alloc1DTex function. The OpenGL texture id given by the
+       glGenTextures function. The space is allocated and parameters (MIN, MAG,
+       WRAP_*) set in the same ways as the Alloc1DTex function.
 
-       The texture data is always GL_UNSIGNED_BYTE, as this function was also intended to be used to
-       allocate multidimensional transfer function data.
+       The texture data is always GL_UNSIGNED_BYTE, as this function was also
+       intended to be used to allocate multidimensional transfer function data.
 
        \param w The width of the image.
        \param h The height of the image
@@ -51,13 +52,15 @@ namespace knl
       /*!
        \brief Allocates a 3D texture.
 
-       This function allocates a 3D texture in the OpenGL device. This is done by getting a name from
-       the context, setting the texture parameters (MIN, MAG filters and WRAP_*) and calling the
-       glTexImage3D with a NULL pointer. The format and type of the data is determined using the
-       bytes_elem parameter. The type may be GL_UNSIGNED_(BYTE,SHORT,INT) and ther format may be
-       GL_R8, GL_R16 and GL_R32F respectively.
+       This function allocates a 3D texture in the OpenGL device. This is done
+       by getting a name from the context, setting the texture parameters (MIN,
+       MAG filters and WRAP_*) and calling the glTexImage3D with a NULL pointer.
+       The format and type of the data is determined using the bytes_elem
+       parameter. The type may be GL_UNSIGNED_(BYTE,SHORT,INT) and ther format
+       may be GL_R8, GL_R16 and GL_R32F respectively.
 
-       This function was intended to be used to allocate the space for a dataset in the GPU.
+       This function was intended to be used to allocate the space for a dataset
+       in the GPU.
 
        \param w The width of the volume
        \param h The height of the volume
@@ -69,15 +72,24 @@ namespace knl
       GLuint Alloc3DTex(size_t w, size_t h, size_t slices, size_t bytes_elem);
 
       /*!
-       \brief
+       \brief Uploads a 1D texture to the OpenGL device.
 
-       \param w
-       \param num_channels
-       \param data
-       \param tex_id
-       \return bool
+       This function allocates and uploads a 1D texture in the OpenGL device.
+       This is done by receiving a valid texture name (via the tex_id parameter)
+       queriyng the internal format and channel format using the num_channels
+       parameter and uploading the data.
+
+       The texture data is always GL_UNSIGNED_BYTE. This function is used mainly
+       to allocate transfer function data.
+
+       \param w The width of the texture
+       \param num_channels The number of channels
+       \param data Pointer to the data
+       \param tex_id The texture name
+       \return bool True if the texture was uploaded successfully, false
+               otherwise
       */
-      bool Upload1DData(size_t w, size_t num_channels, void* io, GLuint tex_id);
+      bool Upload1DData(size_t w, size_t num_channels, void* data, GLuint tex_id);
 
       /*!
        \brief
@@ -89,7 +101,7 @@ namespace knl
        \param tex_id
        \return bool
       */
-      bool Upload2DData(size_t w, size_t h, size_t num_channels, void* io, GLuint tex_id);
+      bool Upload2DData(size_t w, size_t h, size_t num_channels, void* data, GLuint tex_id);
 
       /*!
        \brief
@@ -102,7 +114,7 @@ namespace knl
        \param tex_id
        \return bool
       */
-      bool Upload3DData(size_t w, size_t h, size_t slices, size_t bytes_elem, void* io, GLuint tex_id);
+      bool Upload3DData(size_t w, size_t h, size_t slices, size_t bytes_elem, void* data, GLuint tex_id);
     }
   }
 }
