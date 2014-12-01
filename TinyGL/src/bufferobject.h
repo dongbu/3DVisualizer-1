c31c32b7b33a6fe9ad863c94e73566b984aa594f
Guilherme Schardong <schardon@tecgraf.puc-rio.br>
2014-12-01 14:19:25 -0200

    Documented the BufferObject and FramebufferObject classes.

--- TinyGL/src/bufferobject.h
+++ TinyGL/src/bufferobject.h
@@ -4,10 +4,15 @@
 #include <GL/glew.h>
 
 /**
- * Class BufferObject
+ * @brief The BufferObject class Abstracts the creation and management of
+ * buffer objects.
+ *
  * This class serves the purpose of abstracting the creation and management of
  * buffer objects of any kind. The user must define the target, size and usage
  * parameters using OpenGL values.
+ *
+ * The storage for the buffer object is allocated upon construction. However,
+ * user may allocate the storage again, should he need it.
  */
 class BufferObject
 {
@@ -15,7 +20,29 @@ public:
   BufferObject(GLenum target, size_t buff_size, GLenum usage);
   ~BufferObject();
 
+  /**
+   * @brief allocateStorage Allocates the storage space in the OpenGL device.
+   *
+   * This method calls the glBufferData function passing the target and usage
+   * provided during the construction and the size provided by the buff_size
+   * parameter.
+   *
+   * The buffer must be set as current first by using the bind() method.
+   *
+   * @param buff_size The size in bytesof the buffer to be allocated.
+   */
   void allocateStorage(size_t buff_size);
+
+  /**
+   * @brief sendData Uploads the data to the OpenGL device.
+   *
+   * This method allocates (if not previously allocated) the space in the
+   * OpenGL context and uploads the data.
+   *
+   * The buffer must be set as current first by using the bind() method.
+   *
+   * @param data A pointer to the data in the memory.
+   */
   void sendData(GLvoid* data);
 
   void bind();
--- TinyGL/src/framebufferobject.h
+++ TinyGL/src/framebufferobject.h
@@ -4,13 +4,42 @@
 #include <map>
 #include <GL/glew.h>
 
+/**
+ * @brief The FramebufferObject class  Abstracts the creation and management of
+ * framebuffer objects.
+ *
+ * This class is intended to facilitate the creating and management of
+ * OpenGL framebuffer objects. The user may attach a series of renderbuffers or
+ * textures to act as the color/depth/stencil buffers. All of the attached
+ * buffers must be created and bound before the call to attach*Buffer.
+ */
 class FramebufferObject
 {
 public:
   FramebufferObject();
   ~FramebufferObject();
 
+  /**
+   * @brief attachTexBuffer Calls the glFramebufferTexture function to attach
+   * a texture to the currently bound framebuffer.
+   * @param target The framebuffer target: GL_DRAW_FRAMEBUFFER,
+   * GL_READ_FRAMEBUFFER, or GL_FRAMEBUFFER
+   * @param attach The framebuffer attachment point: GL_COLOR_ATTACHMENTi,
+   * GL_DEPTH_ATTACHMENT, GL_STENCIL_ATTACHMENT or GL_DEPTH_STENCIL_ATTACHMENT
+   * @param texid The texture name given by the OpenGL context
+   * @param level The texture mipmap level
+   */
   void attachTexBuffer(GLenum target, GLenum attach, GLuint texid, GLint level);
+
+  /**
+   * @brief attachRenderBuffer calls the glFramebufferRenderbuffer to attach
+   * a renderbuffer to the currently bound framebuffer.
+   * @param target The framebuffer target: GL_DRAW_FRAMEBUFFER,
+   * GL_READ_FRAMEBUFFER, or GL_FRAMEBUFFER
+   * @param attach Specifies the framebuffer attachment point
+   * @param rbtarg The renderbuffer target. Always GL_RENDERBUFFER
+   * @param rbid The renderbuffer name given by the OpenGL context
+   */
   void attachRenderBuffer(GLenum target, GLenum attach, GLenum rbtarg, GLuint rbid); 
 
   void bind(GLenum target)
@@ -30,6 +59,10 @@ public:
     return 0;
   }
 
+  /**
+   * @brief checkStatus Checks if there were any errors with the framebuffer
+   * creation and prints them on the log.
+   */
   void checkStatus();
 
 private:
